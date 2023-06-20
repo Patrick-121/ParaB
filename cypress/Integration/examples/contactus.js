af97f1e2301fd3dfc3712f9c2a011303cbbdfa27
Patrick Tshimpuki <patrick.tshimpuki@lendscape.com>
2023-06-20 23:16:05 +0100

    Contact us branch

---
+++ cypress/Integration/examples/contactus.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+describe ("Navigate to home page",function() {
+
+it ("Navigate to login page and contact us page", function(){
+    cy.visit("https://parabank.parasoft.com/parabank/index.htm")
+    cy.contains("Contact Us").click()
+    cy.get("input[id = 'name']").type("Patrick Tshimpuki")
+    cy.get("input[id = 'email']").type("ptshimpuki.yahoo.co.uk")
+    cy.get("input[id = 'phone']").type("0794563232")
+    cy.get("textarea[id = 'message']").type("This is a test message")
+    cy.get("input[value = 'Send to Customer Care']").click()
+})
+})
